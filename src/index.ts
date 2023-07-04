198531a18745d84daa6d9c63111de32dbd9041ce
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-07-04 02:20:19 -0700

    feat: finish index.ts file, finish  task

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,57 @@
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
-type Task ={
-  id: string,
-    title: string,
-    completed: boolean,
-    createdAt: Date
-}
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
 const list = document.querySelector<HTMLUListElement>("#list");
-// const form = document.querySelector<HTMLFormElement>("#new-task-form");
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
-form?.addEventListener("submit", e => {
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+form?.addEventListener("submit", (e) => {
   e.preventDefault();
   // check that element not equal null
-  if(input?.value == "" || input?.value == null) return;
-  
+  if (input?.value == "" || input?.value == null) return;
+
   const newTask: Task = {
     id: uuidv4(),
     title: input.value,
     completed: false,
-    createdAt: new Date()
-  }
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+
   addListItem(newTask);
-})
+  input.value = "";
+});
 function addListItem(task: Task) {
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    // console.log(tasks);
+    saveTasks();
+  });
   checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
 
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
 }
